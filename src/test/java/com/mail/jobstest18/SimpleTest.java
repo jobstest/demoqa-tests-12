c8c7960997c771f8d454540ad6313403449007b6
Vladimir Parfionov <jobstest18@gmail.com>
2022-04-03 18:49:30 +0400

    added test

---
+++ src/test/java/com/mail/jobstest18/SimpleTest.java
@@ -0,0 +1,41 @@
+package com.mail.jobstest18;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.*;
+
+public class SimpleTest {
+
+    @BeforeAll
+    static void initDB(){
+        System.out.println("### @BeforeAll");
+    }
+
+    @BeforeEach
+    void openYaPge() {
+        System.out.println("### @BeforeEach");
+        Selenide.open("https://yandex.ru");
+    }
+
+    @AfterEach
+    void closeYaPage() {
+        System.out.println("### @AfterEach");
+        WebDriverRunner.closeWindow();
+    }
+
+    @AfterAll
+    static void cleanDB(){
+        System.out.println("### @AfterAll");
+    }
+
+    @Test
+    void assertTest1() {
+        System.out.println("Test1");
+    }
+
+    @Test
+    void assertTest2() {
+        System.out.println("Test2");
+    }
+
+}
